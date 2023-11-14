be9eda4d99e3b6b60d26e6972067cc0690b98e8c
Майя Рембо <maya.ossamu@gmail.com>
2023-11-14 23:20:19 +0300

    Добавьте файлы проекта.

---
+++ cpp-lab-3/cpp-lab-3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int differentsum(const int arr[], int index, const int n, int summ, int sump)
+{
+    if (index == n)
+        return abs(summ - sump);
+    else
+    {
+        int s1= differentsum(arr, index + 1, n, summ + arr[index], sump);
+        int s2 = differentsum(arr, index + 1, n, summ, sump + arr[index]);
+        return min(s1,s2);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[32] = {};
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+    }
+    
+    cout << differentsum(arr, 0, n, 0, 0 );
+
+}
+
